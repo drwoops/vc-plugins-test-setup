30028c5ae8bb57b254ff4c81b8a9c463fc9a4c68
drwoops <thedrwoops@gmail.com>
2022-06-10 11:30:45 +0200

    added mysql container

--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-rm -rf server1 server2 proxy
-mkdir server1 server2 proxy
-touch server1/.keep server2/.keep proxy/.keep
+rm -rf server1 server2 proxy mysql
+mkdir server1 server2 proxy mysql
+touch server1/.keep server2/.keep proxy/.keep mysql/.keep
