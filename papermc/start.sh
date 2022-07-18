6108c4968699f548eb9d2a89c6b0009cfe499b22
drwoops <thedrwoops@gmail.com>
2022-07-18 08:15:56 +0200

    update velocity

--- papermc/start.sh
+++ papermc/start.sh
@@ -1,4 +1,5 @@
 cd /srv/paper/data
 test -r eula.txt || echo "eula=true" > eula.txt
 test -r server.properties || cp ../server.properties .
+test -r config/paper-global.yml || { mkdir config; cp ../paper-global.yml config/; }
 java -Xms2G -Xmx2G -jar ../${JAR} --nogui
--- velocity/start.sh
+++ velocity/start.sh
@@ -1,4 +1,5 @@
 cd /srv/velocity
 cd data
 test -r velocity.toml || cp ../velocity.toml .
+test -r forwarding.secret || cp ../forwarding.secret .
 java -jar ../${JAR}
